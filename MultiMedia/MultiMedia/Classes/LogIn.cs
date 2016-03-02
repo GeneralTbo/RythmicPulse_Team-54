5c7a072e694b84dc60d06d39249bae1999c8116a
Mremza <200706535@student.uj.ac.za>
2016-03-02 18:05:17 +0200

    Added files via upload
    Added some important files of the actual project

---
+++ MultiMedia/MultiMedia/Classes/LogIn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiMedia.Classes
+{
+    public class LogIn
+    {
+
+        private string email;
+        private string password, secretPass;
+
+        LogIn( String e, String p)
+        {
+            email = e;
+            password = p;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        
+
+        //CHECK WHETHER THE PASSWORD MATCHES
+        public Boolean checkPassword(String p)
+        {
+            if (p == secretPass)
+            {
+                return true;
+            }
+            else
+                return false; 
+        }
+
+        //READ FROM DATABABE
+        public String readPassword()
+        {
+
+            return secretPass;
+        
+        }
+
+    }
+}
